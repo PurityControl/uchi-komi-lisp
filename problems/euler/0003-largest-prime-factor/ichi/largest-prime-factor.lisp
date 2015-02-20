af1f9026c2a68833e1892c21c61d6d75b6a9fe7f
Craig Ferry <PurityControl@users.noreply.github.com>
2015-02-20 08:59:18 +0000

    implement primes-up-to using 6k+/-1 ideas, performant to ~200k
    - need to think about algorithm that gets primes '1k' jumps at a time
      - then can for prime factor in small increments

--- problems/euler/0003-largest-prime-factor/ichi/largest-prime-factor.lisp
+++ problems/euler/0003-largest-prime-factor/ichi/largest-prime-factor.lisp
@@ -4,26 +4,35 @@
   (:export #:largest-factor #:primes-to))
 (in-package #:largest-prime-factor)
 
+(defun seive (primes bucket)
+  (if (null bucket)
+      primes
+    (seive
+     (cons (car bucket) primes)
+     (remove-if #'(lambda (x) (zerop (mod x (car bucket)))) (cdr bucket)))))
+
 (defun primes-to (number)
   "determine the largest prime factor for number"
   (labels ((natural-numbers (number)
-                  (loop for x from 2 to number
-                        collect x))
-           (divisible-p (factor)
-                        (lambda (x) (zerop (mod x factor))))
-           (seive (primes bucket)
-                  (if (null bucket)
-                      primes
-                    (let* ((prime (car bucket))
-                           (new-bucket (remove-if (divisible-p prime) (cdr bucket))))
-                      (seive (cons prime primes)
-                             new-bucket)))))
+                  (coerce (loop for x from 3 to number in 2 collect x) 'vector)))
           (let ((number-bucket (natural-numbers number)))
-            (seive '() number-bucket))))
+            (seive '(2) number-bucket))))
 
 (defun largest-factor (number)
   (let* ((primes (copy-list (primes-to number)))
-         (prime-factors 
+         (prime-factor
           (remove-if-not #'(lambda (x) (zerop (mod number x))) primes)))
     (print prime-factors)))
- 
+
+
+(defun primes-up-to (number k-val primes)
+  (let ((test-for-prime (list (1- (* k-val 6)) (1+ (* k-val 6)))))
+    (labels ((is-prime (number)
+	       (notany (lambda (x) (zerop (mod number x))) primes)))
+      (let ((new-primes 
+	     (append
+	      primes
+	      (remove-if-not #'is-prime test-for-prime))))
+	(if (> (1+ (* k-val 6)) number)
+	    new-primes
+	    (primes-up-to number (1+ k-val) new-primes))))))
