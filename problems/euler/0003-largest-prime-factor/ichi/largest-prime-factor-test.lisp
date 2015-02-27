c8ce646f83791aca378104a1175bd2ffb89c36b1
Craig Ferry <PurityControl@users.noreply.github.com>
2015-02-27 16:24:19 +0000

    first implementation working and performant

--- problems/euler/0003-largest-prime-factor/ichi/largest-prime-factor-test.lisp
+++ problems/euler/0003-largest-prime-factor/ichi/largest-prime-factor-test.lisp
@@ -24,13 +24,25 @@
     '(2 3 5 7 11 13 17 19))))
 
 (define-test first-factor-for-6
-  (assert-equal 2 (largest-prime-factor:first-factor 6)))
+  (assert-equal 2 (largest-prime-factor:first-factor 6 1 '(2 3))))
 
 (define-test first-factor-for-7
-  (assert-equal nil (largest-prime-factor:first-factor 7)))
+  (assert-equal 7 (largest-prime-factor:first-factor 7 1 '(2 3))))
+
+(define-test first-factor-for-9
+  (assert-equal 3 (largest-prime-factor:first-factor 9 1 '(2 3))))
+
+(define-test first-factor-for-47
+  (assert-equal 47 (largest-prime-factor:first-factor 47 1 '(2 3))))
 
 (define-test largest-prime-for-13195
   (assert-equal 29 (largest-prime-factor:largest-factor 13195)))
+
+(define-test accumulate-factors-for-6
+  (assert-equal 3 (largest-prime-factor:accumulate-factors 6 '(2 3))))
+
+(define-test accumulate-factors-for-18
+  (assert-equal 3 (largest-prime-factor:accumulate-factors 18 '(2 3))))
 
 (define-test largest-prime-for-600851475143
   (assert-equal 6857 (largest-prime-factor:largest-factor 600851475143)))
--- problems/euler/0003-largest-prime-factor/ichi/largest-prime-factor.lisp
+++ problems/euler/0003-largest-prime-factor/ichi/largest-prime-factor.lisp
@@ -1,15 +1,12 @@
 (defpackage #:largest-prime-factor
   (:use #:cl)
   (:export #:largest-factor #:prime? #:kvals-from-k #:primes-from-k
-	   #:first-factor))
+	   #:first-factor #:accumulate-factors))
 (in-package #:largest-prime-factor)
 
 (defparameter *k* 1)
 (defparameter *primes* '(2 3))
-
-(defun largest-factor (number)
-  ;; TODO implement me
-  number)
+(defparameter *prime-factors* '())
 
 (defun prime? (number previous-primes)
   ;;TODO only check up to sqaure root of primes
@@ -21,6 +18,25 @@
 (defun primes-from-k (k-val primes)
   (remove-if-not #'(lambda (x) (prime? x primes)) (kvals-from-k k-val)))
 
-(defun first-factor (number)
-  (find-if #'(lambda (x) number (zerop (mod number x))) *primes*))
+(defun first-factor (number k lo-primes)
+  (let ((factor (find-if #'(lambda (x) (zerop (mod number x))) lo-primes)))
+    (if factor 
+	factor
+	(first-factor number 
+		      (1+ k) 
+		      (append (primes-from-k k lo-primes) lo-primes)))))
+  
+(defun accumulate-factors (number prime-factors)
+  (let ((prime-factor-product (apply #'* prime-factors)))
+    (if (eq prime-factor-product number)
+	(reduce #'max prime-factors)
+	(accumulate-factors 
+	 number 
+	 (cons 
+	  (first-factor (/ number prime-factor-product) 1 '(2 3))
+	  prime-factors)))))
 
+
+(defun largest-factor (number)
+  ;; TODO implement me
+  (accumulate-factors number '()))
