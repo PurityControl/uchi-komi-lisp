76e32e749aa5d75c8a621c969c69d63bc3820d2d
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-01 11:56:50 +0000

    implementation of products

---
+++ problems/euler/0004-largest-palindrome-product/ichi/largest-palindrome-product-test.lisp
@@ -0,0 +1,28 @@
+(ql:quickload "lisp-unit")
+
+(defpackage #:largest-palindrome-product-test
+  (:use #:common-lisp #:lisp-unit))
+
+(load "largest-palindrome-product")
+
+(in-package #:largest-palindrome-product-test)
+
+(define-test product-for-1-to-3
+  (assert-equal '(1 2 3 2 4 6 3 6 9) (largest-palindrome-product:products 1 3)))
+
+(define-test product-for-1-to-5
+  (assert-equal '(1 2 3 4 5 2 4 6 8 10 3 6 9 12 15 4 8 12 16 20 5 10 15 20 25)  
+		(largest-palindrome-product:products 1 5)))
+
+(define-test product-length-for-1-to-3
+  (assert-equal 9 (length (largest-palindrome-product:products 1 3))))
+
+(define-test product-length-for-1-to-10
+  (assert-equal 100 (length (largest-palindrome-product:products 1 10))))
+
+(define-test product-length-for-1-to-20
+  (assert-equal 400 (length (largest-palindrome-product:products 1 20))))
+
+(let ((*print-errors* t)
+      (*print-failures* t))
+  (run-tests :all :largest-palindrome-product-test))
---
+++ problems/euler/0004-largest-palindrome-product/ichi/largest-palindrome-product.lisp
@@ -0,0 +1,14 @@
+(defpackage #:largest-palindrome-product
+  (:use #:cl)
+  (:export #:products))
+(in-package #:largest-palindrome-product)
+
+(defun products (start end)
+  (labels ((acc-prods (pr1 pr2)
+	     (let ((product (* pr1 pr2)))
+	       (cond 
+		 ((and (eq pr1 end) (eq pr2 end)) (list product))
+		 ((eq pr2 end) (cons product (acc-prods (1+ pr1) start)))
+		 (t (cons product (acc-prods pr1 (1+ pr2))))))))
+    (acc-prods start start)))
+
