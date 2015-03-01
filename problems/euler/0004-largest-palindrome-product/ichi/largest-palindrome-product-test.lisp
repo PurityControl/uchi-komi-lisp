0075fcc735192913150cc4952c30abfef7005c2b
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-01 13:10:22 +0000

    lack of tail recursion causing stack problems not size of list
    - made accumulation of list part of function allowing for tail call
      optimisation

--- problems/euler/0004-largest-palindrome-product/ichi/largest-palindrome-product-test.lisp
+++ problems/euler/0004-largest-palindrome-product/ichi/largest-palindrome-product-test.lisp
@@ -8,10 +8,10 @@
 (in-package #:largest-palindrome-product-test)
 
 (define-test product-for-1-to-3
-  (assert-equal '(1 2 3 2 4 6 3 6 9) (largest-palindrome-product:products 1 3)))
+  (assert-equal '(9 6 3 6 4 2 3 2 1) (largest-palindrome-product:products 1 3)))
 
 (define-test product-for-1-to-5
-  (assert-equal '(1 2 3 4 5 2 4 6 8 10 3 6 9 12 15 4 8 12 16 20 5 10 15 20 25)  
+  (assert-equal '(25 20 15 10 5 20 16 12 8 4 15 12 9 6 3 10 8 6 4 2 5 4 3 2 1)  
 		(largest-palindrome-product:products 1 5)))
 
 (define-test product-length-for-1-to-3
@@ -33,9 +33,9 @@
   (assert-equal 9009 
 		(largest-palindrome-product:largest-palindrome-product 10 99)))
 
-;(define-test largest-palindrome-product-3-digit
-;  (assert-equal 906609 
-;		(largest-palindrome-product:largest-palindrome-product 100 999)))
+(define-test largest-palindrome-product-3-digit
+  (assert-equal 906609 
+		(largest-palindrome-product:largest-palindrome-product 100 999)))
 
 (let ((*print-errors* t)
       (*print-failures* t))
--- problems/euler/0004-largest-palindrome-product/ichi/largest-palindrome-product.lisp
+++ problems/euler/0004-largest-palindrome-product/ichi/largest-palindrome-product.lisp
@@ -4,13 +4,14 @@
 (in-package #:largest-palindrome-product)
 
 (defun products (start end)
-  (labels ((acc-prods (pr1 pr2)
+  (labels ((acc-prods (pr1 pr2 products)
 	     (let ((product (* pr1 pr2)))
 	       (cond 
-		 ((and (eq pr1 end) (eq pr2 end)) (list product))
-		 ((eq pr2 end) (cons product (acc-prods (1+ pr1) start)))
-		 (t (cons product (acc-prods pr1 (1+ pr2))))))))
-    (acc-prods start start)))
+		 ((and (eq pr1 end) (eq pr2 end)) (cons product products))
+		 ((eq pr2 end) 
+		  (acc-prods (1+ pr1) start (cons product products)))
+		 (t (acc-prods pr1 (1+ pr2) (cons product products)))))))
+    (acc-prods start start '())))
 
 (defun palindrome-p (number)
   (let ((num (write-to-string number)))
