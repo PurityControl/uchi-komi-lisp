8e3ceedcb784994eedfbe34ffef4b041180389f4
Craig Ferry <PurityControl@users.noreply.github.com>
2015-03-02 14:01:35 +0000

    implement smallest-multiple-for function

--- problems/euler/0005-smallest-multiple/ichi/smallest-multiple.lisp
+++ problems/euler/0005-smallest-multiple/ichi/smallest-multiple.lisp
@@ -1,7 +1,15 @@
 (defpackage #:smallest-multiple
   (:use #:cl)
-  (:export #:common-multiples-p))
+  (:export #:common-multiples-p #:smallest-multiple-for))
 (in-package #:smallest-multiple)
 
 (defun common-multiples-p (number lo-numbers)
   (every #'(lambda (x) (zerop (mod number x))) lo-numbers))
+
+(defun smallest-multiple-for (lo-numbers)
+  (let ((max-num (apply #'max lo-numbers)))
+    (labels ((smallest-multiple (lo-numbers multiple)
+	       (if (common-multiples-p multiple lo-numbers)
+		   multiple
+		   (smallest-multiple lo-numbers (+ max-num multiple)))))
+    (smallest-multiple lo-numbers max-num))))
